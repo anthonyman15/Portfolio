38f80706bc91ea00615dc1f1730a2d66e28b6128
Ka Pui Man <70656599+fairywizard15@users.noreply.github.com>
2022-06-17 03:07:41 +0800

    navi-fixed

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,9 @@
-const menuBtn = document.querySelector('.menu-btn');
-let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-    if (!menuOpen) {
-        menuBtn.classList.add('open');
-        menuOpen = true;
-    } else {
-        menuBtn.classList.remove('open');
-        menuOpen = false;
-    }
-});
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
