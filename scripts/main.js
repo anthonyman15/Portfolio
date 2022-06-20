5613e81ec39c7835a39bab1ebae0bfe91d77e907
Ka Pui Man <70656599+fairywizard15@users.noreply.github.com>
2022-06-20 08:40:54 +0800

    homepage, mobile bur version done

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const backgroundBlur = document.querySelector(".banner");
 
 hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
+    backgroundBlur.classList.toggle("active");
 }
